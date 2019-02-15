dc979085dd8952744e43ad484f786f5f46a8a427
fallGamlet <fallgamlet@gmail.com>
2019-02-15 12:19:05 +0200

    add Tax 20

--- src/main/java/ru/evotor/framework/receipt/TaxNumber.kt
+++ src/main/java/ru/evotor/framework/receipt/TaxNumber.kt
@@ -3,6 +3,7 @@ package ru.evotor.framework.receipt
 import java.math.BigDecimal
 
 enum class TaxNumber(val value: BigDecimal) {
+    VAT_20(BigDecimal.valueOf(20)),
     VAT_18(BigDecimal.valueOf(18)),
     VAT_10(BigDecimal.valueOf(10)),
     NO_VAT(BigDecimal.ZERO),
